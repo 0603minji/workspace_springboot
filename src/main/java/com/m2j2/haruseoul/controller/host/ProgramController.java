c08caa9ac2b2b18d577439adf0913b191a70e8d1
minji <qs1018@naver.com>
2024-09-08 16:05:23 +0900

    program list 호출

--- src/main/java/com/m2j2/haruseoul/controller/host/ProgramController.java
+++ src/main/java/com/m2j2/haruseoul/controller/host/ProgramController.java
@@ -2,19 +2,22 @@ package com.m2j2.haruseoul.controller.host;
 
 import com.m2j2.haruseoul.entity.Program;
 import com.m2j2.haruseoul.entity.ProgramView;
-import com.m2j2.haruseoul.service.ProgramService.ProgramService;
+import com.m2j2.haruseoul.service.host.ProgramService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping("host/program")
-public class ProgramController {;
+@RequestMapping(value = "/host/program")
+public class ProgramController {
+
+    @Autowired
+    private ProgramService programService;
 
     @GetMapping("info/create")
     public String info() {
@@ -46,23 +49,21 @@ public class ProgramController {;
         return "host/program/create/schedule";
     }
 
-
-    @Autowired
-    private ProgramService programService;
-
-    @GetMapping("list")
+    @GetMapping("/list")
     public String list(
-            @RequestParam(value = "hostId",required = true) Long hostId,
-            @RequestParam(value = "category", required = false) List<String> categories,
+            @RequestParam(value = "category", required = false) List<String> category,
             Model model) {
-        List<ProgramView> programs = programService.getList(hostId, categories);
+        System.out.println("/list controller 호출");
 
-        System.out.println(categories);
-        System.out.println(programs);
-
-        model.addAttribute("programs", programs);
-        model.addAttribute("hostId", hostId);
-        return "host/program/list";
+        /**
+         * todo. 컨트롤러의 역할
+         * 1. 파라미터(@RequestParam) validation 검사
+         *      ex. 음수가 들어오지는 않았는지
+         *          2개 들어와야하는데 3개가 들어오지는 않았는지
+         * 2. 서비스를 호출한다.
+         * 3. 서비스가 준 값을 반환한다.
+          */
+        model.addAttribute("programs", programService.getCategory(category));
+        return "host/program/list";         // 페이지 반환
     }
-
 }
---
+++ src/test/java/com/m2j2/haruseoul/controller/host/ProgramControllerTest.java
@@ -0,0 +1,7 @@
+package com.m2j2.haruseoul.controller.host;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProgramControllerTest {
+
+}
